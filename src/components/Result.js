359a3d82f04327c7b71d71abdcf766c64467d1cc
V.Sahanashre <sahanashre.v@kalvium.community>
2024-02-21 13:30:18 +0530

    Added stylings

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,11 +3,26 @@ import quizicon from "../Assets/quizicon.png";
 import { useLocation } from 'react-router-dom';
 import {Link} from "react-router-dom";
 import "../App.css";
+import {useState} from "react"
 
 export default function Result() {
   let Location = useLocation()
   const myProp = Location.state 
-console.log(myProp.score)
+// console.log(myProp.score)
+
+
+const [theme, setTheme] = useState("Light");
+    
+const handleTheme = () => {
+    if (theme === "Light") {
+        setTheme("Dark");
+        document.body.style.backgroundColor = "black";
+    } 
+    else {
+        setTheme("Light");
+        document.body.style.backgroundColor = "white";
+    }
+}
 
   return (
     <div>
@@ -15,7 +30,7 @@ console.log(myProp.score)
 
 <div className="nav">
           <img className="imgicon" src={quizicon} alt="" />
-          <button className="dark">Dark</button>
+          <button className="dark" onClick={handleTheme}>{theme}</button>
           </div>
 
           <h1 className="center">Final Results</h1>
